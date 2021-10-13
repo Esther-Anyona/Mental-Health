a579242bcd597990fc37b1a2dd7f1481a64751ae
mwikalikyalo <winifred.kyalo@student.moringaschool.com>
2021-10-13 20:36:07 +0300

    Changes made to the js.file

--- form.js
+++ form.js
@@ -1,8 +1,22 @@
 //get modal element
-var modal= document.getElementById("button");
+var modal= document.getElementById('simpleModal');
 
 //get the open modal button
-var modalBtn= document.getElementById("submit");
+var modalBtn= document.getElementById('modalBtn');
 
 //get close button
-var closeBtn= document.getElementbyId("closeBtn");
+var closeBtn= document.getElementbyClassName('closeBtn')[0];
+
+//listen for open click
+modalBtn.addEventListener('click', openModal);
+//listen for close click
+closeBtn.addEventListener('click', closeModal);
+
+//function to open modal
+function openModal(){
+  modal.style.display='block';
+}
+//function to close modal
+function closeModal(){
+  modal.style.display='none';
+}

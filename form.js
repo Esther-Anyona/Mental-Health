6e73699a829e502f56520f81ee8c383aa7fda33d
mwikalikyalo <winifred.kyalo@student.moringaschool.com>
2021-10-13 13:01:21 +0300

    Add javascript for the button

---
+++ form.js
@@ -0,0 +1,8 @@
+//get modal element
+var modal= document.getElementById("button");
+
+//get the open modal button
+var modalBtn= document.getElementById("submit");
+
+//get close button
+var closeBtn= document.getElementbyId("closeBtn");
